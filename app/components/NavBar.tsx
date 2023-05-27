3327eba34e5c843ed9a671d7722b97337ead8da5
AnthonyMejia0 <mejia.anthony96@gmail.com>
2023-05-26 19:28:54 -0700

    added mobile menu

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,6 +3,8 @@
 import { signOut, useSession } from "next-auth/react";
 import React, { useState } from "react";
 import Modal from "./Modal";
+import { AiOutlineMenu } from "react-icons/ai";
+import { IoMdClose } from "react-icons/io";
 
 interface NavProps {
   getLists: () => Promise<void>;
@@ -12,6 +14,7 @@ function NavBar({ getLists }: NavProps) {
   const { data: session } = useSession();
   const user = session?.user;
   const [isOpen, setIsOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const createNewList = async (value: string) => {
     const response = await fetch(`/api/lists/${user?.id}`, {
@@ -33,7 +36,7 @@ function NavBar({ getLists }: NavProps) {
   };
 
   return (
-    <nav className="sticky top-0 flex h-16 w-full items-center justify-between bg-gray-200 px-5 py-3 shadow-md">
+    <nav className="sticky top-0 z-10 flex h-16 w-full items-center justify-between bg-gray-200 px-5 py-3 shadow-md">
       <Modal
         handleSubmit={createNewList}
         title="Let's create a new list"
@@ -43,10 +46,10 @@ function NavBar({ getLists }: NavProps) {
         setIsOpen={setIsOpen}
       />
 
-      <p className="text-xl">
-        Welcome <span className="hidden xl:inline-block">{user?.name}</span>
+      <p className="md:text-xl">
+        Welcome <span className="">{user?.name}</span>
       </p>
-      <div className="space-x-2">
+      <div className="hidden space-x-2 md:inline-block">
         <button
           onClick={() => setIsOpen(true)}
           className="rounded-full bg-white px-3 py-1 font-medium text-black hover:bg-gray-300"
@@ -60,6 +63,30 @@ function NavBar({ getLists }: NavProps) {
           Sign Out
         </button>
       </div>
+
+      <button onClick={() => setMenuOpen(!menuOpen)} className="md:hidden">
+        {menuOpen ? <IoMdClose size={28} /> : <AiOutlineMenu size={28} />}
+      </button>
+
+      {menuOpen && (
+        <div className="absolute right-4 top-[calc(100%+1rem)] flex flex-col space-y-2 rounded-md bg-gray-200 p-4 shadow-md">
+          <button
+            onClick={() => {
+              setIsOpen(true);
+              setMenuOpen(false);
+            }}
+            className="bg-white px-3 py-1 font-medium text-black hover:bg-gray-300"
+          >
+            New List
+          </button>
+          <button
+            onClick={() => signOut()}
+            className="bg-gray-500 px-3 py-1 font-medium text-white hover:bg-gray-600"
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
     </nav>
   );
 }
