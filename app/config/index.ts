fc75e3c11b389c0ee302af0d3ac3ce39a6232474
AnthonyMejia0 <mejia.anthony96@gmail.com>
2023-05-30 01:55:13 -0700

    changed export name

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,7 +1,9 @@
-export default {
+const dbConfig = {
   HOST: process.env.DB_HOST,
   PORT: process.env.DB_PORT,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
   NAME: process.env.DB_NAME,
 };
+
+export default dbConfig;
