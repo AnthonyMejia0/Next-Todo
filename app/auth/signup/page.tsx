3fff636b2ab9994ea68e0c41d696dca91942dbd5
AnthonyMejia0 <mejia.anthony96@gmail.com>
2023-05-30 01:13:17 -0700

    created signup page

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const { push } = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError(null);
+
+    const response = await fetch("/api/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+
+    if (response.ok) {
+      const signInResponse = await signIn("credentials", {
+        email: email,
+        password: password,
+        redirect: false,
+      });
+
+      if (signInResponse?.error) {
+        push("/auth/login");
+      } else {
+        push("/");
+      }
+    } else {
+      const data = await response.json();
+      setError(data.error);
+    }
+
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <main className="flex h-screen w-screen items-center justify-center bg-white text-white">
+      <div className="w-[85%] rounded-md bg-gray-500 p-7 md:w-auto md:px-12 md:py-7">
+        <h2 className="mb-5 text-center text-xl font-bold md:text-3xl">
+          Create an Account
+        </h2>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col space-y-4 text-black"
+        >
+          <input
+            className="rounded p-1 md:p-2"
+            type="text"
+            placeholder="First name"
+            value={name}
+            onChange={(e) => setName(e.currentTarget.value)}
+            required
+            autoFocus
+          />
+          <input
+            className="rounded p-1 md:p-2"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.currentTarget.value)}
+            required
+          />
+          <input
+            className="rounded p-1 md:p-2"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.currentTarget.value)}
+            required
+          />
+          <button
+            type="submit"
+            className="w-full rounded bg-gray-400 py-1 text-white hover:bg-gray-300 md:py-2"
+          >
+            Signup
+          </button>
+        </form>
+
+        {error && <p className="text-red-500">{error}</p>}
+
+        <div className="text-center">
+          <p className="mt-2 text-sm text-white md:text-base">
+            Have an account?
+            <Link
+              className="ml-2 text-sm text-gray-300 hover:text-gray-200"
+              href="/"
+            >
+              Sign in.
+            </Link>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Signup;
