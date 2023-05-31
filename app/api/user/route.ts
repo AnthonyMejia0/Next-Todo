27239163a648f1715d0797d1c7dcd9fb0ad8939a
AnthonyMejia0 <mejia.anthony96@gmail.com>
2023-05-30 22:32:35 -0700

    added logs

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -52,6 +52,7 @@ export async function POST(request: Request) {
     //   status: 200,
     // });
   } catch (error) {
+    console.log(error);
     return NextResponse.json({ error: "Invalid data" }, { status: 400 });
   }
 }
