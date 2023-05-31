2efd1f152dba7bea69222789de2a390e9c79e81e
AnthonyMejia0 <mejia.anthony96@gmail.com>
2023-05-30 22:35:59 -0700

    removed logs

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -10,49 +10,43 @@ interface RequestBody {
 }
 
 export async function POST(request: Request) {
-  try {
-    const body: RequestBody = await request.json();
-    console.log(body.name, body.email, body.password);
+  const body: RequestBody = await request.json();
 
-    const user = await User.findOne({
-      where: {
-        email: body.email,
+  const user = await User.findOne({
+    where: {
+      email: body.email,
+    },
+  });
+
+  if (user) {
+    return NextResponse.json(
+      {
+        error: "User already exists",
       },
-    });
-
-    if (user) {
-      return NextResponse.json(
-        {
-          error: "User already exists",
-        },
-        {
-          status: 403,
-        }
-      );
-    }
-
-    // const salt = bcrypt.genSaltSync(10);
-    // const hash = bcrypt.hashSync(body.password, salt);
-
-    // const newUser = await User.create({
-    //   name: body.name,
-    //   email: body.email,
-    //   hashed_password: hash,
-    // });
-
-    // const { hashed_password, ...userWithoutPass } = newUser.dataValues;
-    // const accessToken = signJwtAccessToken(userWithoutPass);
-
-    // const results = {
-    //   ...userWithoutPass,
-    //   accessToken,
-    // };
-
-    // return NextResponse.json(results, {
-    //   status: 200,
-    // });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json({ error: "Invalid data" }, { status: 400 });
+      {
+        status: 403,
+      }
+    );
   }
+
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(body.password, salt);
+
+  const newUser = await User.create({
+    name: body.name,
+    email: body.email,
+    hashed_password: hash,
+  });
+
+  const { hashed_password, ...userWithoutPass } = newUser.dataValues;
+  const accessToken = signJwtAccessToken(userWithoutPass);
+
+  const results = {
+    ...userWithoutPass,
+    accessToken,
+  };
+
+  return NextResponse.json(results, {
+    status: 200,
+  });
 }
